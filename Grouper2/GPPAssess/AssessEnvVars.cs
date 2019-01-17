0e10fe5189c85e813982b9d96e18f0655fab6ec6
Mike Loss <mike.loss@asteriskinfosec.com.au>
2019-01-17 07:27:20 +0000

    Did environment variables assess method

--- Grouper2/GPPAssess/AssessEnvVars.cs
+++ Grouper2/GPPAssess/AssessEnvVars.cs
@@ -6,15 +6,47 @@ namespace Grouper2
     {
         private JObject GetAssessedEnvironmentVariables(JObject gppCategory)
         {
-            int interestLevel = 0;
-            JProperty gppEVProp = new JProperty("EnvironmentVariable", gppCategory["EnvironmentVariable"]);
-            JObject assessedGppEVs = new JObject(gppEVProp);
+
+            int interestLevel = 1;
+
             if (interestLevel < GlobalVar.IntLevelToShow)
             {
-                assessedGppEVs = new JObject();
+                return null;
             }
 
-            return assessedGppEVs;
+            JObject assessedGppEvs = new JObject();
+
+            if (gppCategory["EnvironmentVariable"] is JArray)
+            {
+                foreach (JToken gppEv in gppCategory["EnvironmentVariable"])
+                {
+                    JObject assessedGppEv = AssessGppEv(gppEv);
+                    string uid = assessedGppEv["UID"].ToString();
+                    assessedGppEvs.Add(uid, assessedGppEv);
+                }
+            }
+            else
+            {
+                JObject assessedGppEv = AssessGppEv(gppCategory["EnvironmentVariable"]);
+                string uid = assessedGppEv["UID"].ToString();
+                assessedGppEv["UID"].Remove();
+                assessedGppEvs.Add(uid, assessedGppEv);
+            }
+
+            return assessedGppEvs;
+        }
+
+        static JObject AssessGppEv(JToken gppEv)
+        {
+            JObject AssessedGppEv = new JObject();
+            
+            AssessedGppEv.Add("Name", Utility.GetSafeString(gppEv, "@name"));
+            AssessedGppEv.Add("Status", Utility.GetSafeString(gppEv, "@status"));
+            AssessedGppEv.Add("Changed", Utility.GetSafeString(gppEv, "@changed"));
+            AssessedGppEv.Add("Action", Utility.GetActionString(gppEv["Properties"]["@action"].ToString()));
+            AssessedGppEv.Add("UID", Utility.GetSafeString(gppEv, "@uid"));
+            
+            return AssessedGppEv;
         }
     }
 }
